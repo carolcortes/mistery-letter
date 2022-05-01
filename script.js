57f357119fd0d9ce73c9e181789ce932f4c0b1c2
carolcortes <caroline.ocortes@gmail.com>
2022-05-01 11:36:17 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log('Olá, mundo!');
+
+const createBtn = document.getElementById('criar-carta');
+const input = document.getElementById('carta-texto');
+const card = document.getElementById('carta-gerada');
+
+// create card
+function createCard() {
+  card.innerText = '';
+  const words = input.value.split(' ');
+  if (input.value === '' || input.value === ' ') {
+    card.innerText = 'Por favor, digite o conteúdo da carta';
+  } else {
+    words.forEach((element) => {
+      const wordSpan = document.createElement('span');
+      card.appendChild(wordSpan);
+      wordSpan.innerText = element;
+    });
+  }
+}
+createBtn.addEventListener('click', createCard);
